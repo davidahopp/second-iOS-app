b8106ddf5c7c8890cb26d2c93abf6578aaed415a
Abby Langner <abby.langner@agapered.com>
2012-08-06 18:04:02 -0500

    started map view

---
+++ BirdWatching/BirdWatchingMapViewController.h
@@ -0,0 +1,14 @@
+//
+//  BirdWatchingMapViewController.h
+//  BirdWatching
+//
+//  Created by Abby Langner on 8/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface BirdWatchingMapViewController : UIViewController
+
+@end
